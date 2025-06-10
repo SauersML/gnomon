d7d1fdd037e8b1880f13fdb6d9192f0a043cb520
SauersML <123714540+SauersML@users.noreply.github.com>
2025-06-10 12:51:32 -0500

    Update batch.rs

--- src/batch.rs
+++ src/batch.rs
@@ -19,7 +19,7 @@ use crossbeam_queue::ArrayQueue;
 use rayon::prelude::*;
 use std::cell::RefCell;
 use std::error::Error;
-use std::simd::{cmp::SimdPartialEq, num::SimdUint, Simd, ToBitMask};
+use std::simd::{cmp::SimdPartialEq, num::SimdUint, Simd};
 
 // --- SIMD & Engine Tuning Parameters ---
 const SIMD_LANES: usize = 8;
