452d1c0608daae769debe50e36151b07df6c33a4
SauersML <123714540+SauersML@users.noreply.github.com>
2025-06-17 17:43:13 -0500

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -2,11 +2,10 @@ import Lake
 open Lake DSL
 
 package calibration where
-  srcDir := "proofs"
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.1"
 
 @[default_target]
 lean_lib Calibrator where
-  root := "proofs"
+  srcDir := "proofs"
