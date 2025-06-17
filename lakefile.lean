acd82072f09048c1702ad675947f47018efcc939
SauersML <123714540+SauersML@users.noreply.github.com>
2025-06-17 14:54:07 -0500

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package calibration where
   srcDir := "proofs"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.21.0"
 
 @[default_target]
 lean_lib Calibrator where
