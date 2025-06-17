84cf33a011712c3f539e1be7a625f5af097b3e88
SauersML <123714540+SauersML@users.noreply.github.com>
2025-06-17 14:06:52 -0500

    Create lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,17 @@
+/-!
+=================================================================
+                    Project Configuration
+=================================================================
+-/
+
+import Lake
+open Lake DSL
+
+package `calibration` where
+  srcDir := "proofs"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib `Calibrator` where
