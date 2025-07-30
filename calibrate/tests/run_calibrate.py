ef9db8fc6079e21e7d6313cabf10d84f3367ab7a
SauersML <123714540+SauersML@users.noreply.github.com>
2025-07-30 17:15:15 -0500

    Update run_calibrate.py

--- calibrate/tests/run_calibrate.py
+++ calibrate/tests/run_calibrate.py
@@ -109,7 +109,7 @@ def main():
         "--num-pcs", str(NUM_PCS),
         # These knot/degree values are analogous to the basis complexity in the R code
         "--pgs-knots", "8",  # for 'score' (variable_one)
-        "--pc-knots", "18",   # for 'PC1' (variable_two)
+        "--pc-knots", "8",   # for 'PC1' (variable_two)
         "--pgs-degree", "3",
         "--pc-degree", "3",
         str(RUST_TRAIN_DATA_PATH),
